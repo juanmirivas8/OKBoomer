57398e46492b2866f557dc03241da15494d82838
Roxas57 <78800243+Roxas57@users.noreply.github.com>
2022-03-10 10:09:29 +0100

    xdd

--- src/main/java/interfaces/reservations/IReservationDAO.java
+++ src/main/java/interfaces/reservations/IReservationDAO.java
@@ -1,5 +1,12 @@
 package interfaces.reservations;
 
+import java.util.Collection;
+
+import model.Reservation;
+
 public interface IReservationDAO {
+	Collection<Reservation> listOfReservationByDate();
+	Collection<Reservation> listOfReservationByName();
+	Collection<Reservation> listOfReservationById();
 
 }
