24a4f5945cf3fb4e782bc02c1b5e591c1dce8348
Franpd25 <franprados025@gmail.com>
2022-03-15 12:44:47 +0100

    sprint reservation

--- src/main/java/interfaces/reservations/IReservationDAO.java
+++ src/main/java/interfaces/reservations/IReservationDAO.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import model.Reservation;
 
 public interface IReservationDAO {
+	
 	Collection<Reservation> listOfReservationByDate();
 	Collection<Reservation> listOfReservationByName();
 	Collection<Reservation> listOfReservationById();
